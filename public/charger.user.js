f584a80096615d720d558104ed54488087e4d0ce
Vadim Sabirov <pr0head@gmail.com>
2025-07-23 04:43:56 +0300

    Rename repairer.user.js to charger.user.js and update script to charge points instead of repairing; add charge button with progress handling and team ownership check

--- public/charger.user.js
+++ public/charger.user.js
@@ -1,11 +1,11 @@
 // ==UserScript==
-// @name SBG Point repairer
+// @name SBG Point charger
 // @description Retrieves a list of keys from the inventory and charges each one fully.
 // @version 0.0.1
 // @author https://github.com/pr0head
 // @homepage https://github.com/pr0head/sbg-scripts
-// @downloadURL https://github.com/pr0head/sbg-scripts/raw/refs/heads/main/public/repairer.user.js
-// @updateURL https://github.com/pr0head/sbg-scripts/raw/refs/heads/main/public/repairer.user.js
+// @downloadURL https://github.com/pr0head/sbg-scripts/raw/refs/heads/main/public/charger.user.js
+// @updateURL https://github.com/pr0head/sbg-scripts/raw/refs/heads/main/public/charger.user.js
 // @match https://sbg-game.ru/app/*
 // @grant none
 // @license MIT
@@ -23,27 +23,50 @@
         window.addEventListener('DOMContentLoaded', _ => main());
     }
 
-    const onlyRepairGuard = true; // Заряжать только точки где я владелец
-    const myTeamID = 2;
+    const onlyRepairGuard = true; // Заряжать только точки, где я владелец
+    const defaultButtonValue = 'Зарядить';
+    const progressButtonValue = 'Заряжается';
+
+    let myTeamID = 0;
+    let inProgress = false;
 
     async function main() {
-        console.log("loaded");
-        $('.inventory').find('.inventory__controls').append('<button>Починятор</button>');
-        $(document).on('click', '.inventory__controls button', function() {
-            repair();
+        $.get('https://sbg-game.ru/api/self', async function(data) {
+            if (data.t !== undefined) {
+                myTeamID = data.t
+
+                $('.inventory').find('.inventory__controls').append(`<button id="chargerBtn">${defaultButtonValue}</button>`);
+                $('#chargerBtn').on('click', function() {
+                    if (inProgress) {
+                        window.alert("Зарядка еще не завершена");
+                        return;
+                    }
+
+                    $('#chargerBtn').text(progressButtonValue);
+                    inProgress = true;
+                    repair();
+                });
+            } else {
+                console.error(`My team is undefined`, err);
+            }
+        }).fail(function(err) {
+            console.error(`Network or server error during getting self`, err);
         });
     }
 
     async function repair() {
         $.get('https://sbg-game.ru/api/inventory', async function(data) {
             console.log('Fetching inventory...');
-            const items = data.i.filter(item => item.t == 3);
+            const items = data.i.filter(item => item.t === 3);
 
             for (const item of items) {
                 const delay = Math.floor(Math.random() * 501) + 500;
                 await new Promise(res => setTimeout(res, delay));
                 processKey(item.l, item.ti);
             }
+
+            $('#chargerBtn').text(defaultButtonValue);
+            inProgress = false;
 
             console.log('Done');
         });
@@ -56,12 +79,12 @@
                 return;
             }
 
-            if (data.data.te != myTeamID) {
-                console.log(`My team does't own a point: ${name}`);
+            if (data.data.te !== myTeamID) {
+                console.log(`My team doesn't own a point: ${name}`);
                 return;
             }
 
-            if (data.data.e == myTeamID) {
+            if (data.data.e === myTeamID) {
                 console.log(`Already fully charged: ${name}`);
                 return;
             }
@@ -73,7 +96,6 @@
     function repairPoint(id, name) {
         $.post('https://sbg-game.ru/api/repair', {guid: id, position: [0,0]}, async function(data) {
             if (data && data.data && data.xp) {
-                console.log(`Fixed the point ${name}`);
                 const delay = Math.floor(Math.random() * 501) + 500;
                 await new Promise(res => setTimeout(res, delay));
                 repairPoint(id, name);
@@ -86,7 +108,6 @@
             }
 
             console.error(`Unknown response during point repair ${name}:`, data);
-            return;
         }).fail(function(err) {
             console.error(`Network or server error during point repair ${name}:`, err);
         });
