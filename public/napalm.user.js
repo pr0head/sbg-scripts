6e20d01d4d4cfe4e513a63ce738b88204b601433
Vadim Sabirov <pr0head@gmail.com>
2026-05-05 15:53:03 +0300

    Adjust positioning of napalm block for improved visibility

--- public/napalm.user.js
+++ public/napalm.user.js
@@ -37,7 +37,7 @@
         'X': {'min': 4, 'max': 4}
     };
     // Минимальный поддерживаемый уровень катализаторов для напалма
-    const $block = $('<div style="text-align: center; margin-bottom: 5px; display: none"></div>');
+    const $block = $('<div style="position: absolute; margin-top: -39px; text-align: center; display: none"></div>');
     const observer = new MutationObserver(function(mutations) {
         mutations.forEach(function(mutation) {
             if (mutation.attributeName === 'class') {
